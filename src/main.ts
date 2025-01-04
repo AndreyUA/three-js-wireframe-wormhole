0f8dbd257802c7487799ee9d89c98aa7e025a8df
AndreyUA <infernalsteel666@gmail.com>
2025-01-04 21:03:53 +0200

    initial three js setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,60 @@
 import "./style.css";
+
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Animate
+const tick = () => {
+  // Update objects
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  globalThis.requestAnimationFrame(tick);
+};
+
+// Resize event listener
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+tick();
