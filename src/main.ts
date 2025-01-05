f9edf4426b398ca89344d3f84b76680c46df8c0d
AndreyUA <infernalsteel666@gmail.com>
2025-01-05 22:28:12 +0200

    implemented crosshair object

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,24 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 5;
 scene.add(camera);
 
+// Mouse crosshair
+let mousePosition = new THREE.Vector2(0, 0);
+const crosshairGroup = new THREE.Group();
+crosshairGroup.position.z = -1;
+camera.add(crosshairGroup);
+const crosshairMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+const crosshairGeometry = new THREE.BufferGeometry();
+const crosshairVertices = new Float32Array([0, 0.05, 0, 0, 0.02, 0]);
+crosshairGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(crosshairVertices, 3)
+);
+for (let i = 0; i < 4; i++) {
+  const line = new THREE.Line(crosshairGeometry, crosshairMaterial);
+  line.rotation.z = (i * Math.PI) / 2;
+  crosshairGroup.add(line);
+}
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
